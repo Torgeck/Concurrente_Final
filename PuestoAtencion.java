35492f515b2804aca95698bdef6b991cc96eeb8b
Torgeck <54917534+Torgeck@users.noreply.github.com>
2024-10-21 01:55:04 -0300

    Agregado logica a PA falta asignarle puerta y terminal a pasajero

--- PuestoAtencion.java
+++ PuestoAtencion.java
@@ -1,28 +1,35 @@
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class PuestoAtencion {
 
     private static int ID = 0;
     private int idPuesto;
     private int capacidadMax;
-    private int capacidadActual;
     private Semaphore colaDisponibilidad;
     private Semaphore pasajeroListo;
     private Semaphore mutexEmpleado;
     private Semaphore mostrador;
-    private ReentrantLock lock;
     private Empleado empleado;
+    private WalkieTalkie walkie;
 
-    public PuestoAtencion(int max) {
+    public PuestoAtencion(int max, WalkieTalkie walkieGuardia) {
         this.idPuesto = generarID();
         this.capacidadMax = max;
-        this.capacidadActual = max;
-        this.colaDisponibilidad = new Semaphore(capacidadMax, true);
+        this.colaDisponibilidad = new Semaphore(max, true);
         this.mostrador = new Semaphore(1);
         this.mutexEmpleado = new Semaphore(0);
         this.pasajeroListo = new Semaphore(0);
-        this.empleado = new Empleado(this);
+        this.walkie = walkieGuardia;
+    }
+
+    public int getIdPuesto() {
+        return this.idPuesto;
+    }
+
+    public boolean setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+        return true;
     }
 
     private int generarID() {
@@ -36,28 +43,46 @@ public class PuestoAtencion {
         try {
             System.out.println("Empleado " + this.empleado.getIdEmpleado() + " en puesto de atencion[" + this.idPuesto
                     + "] esperando pasajeros para atender");
+
             mutexEmpleado.acquire();
+            System.out.println("== Empleado[" + this.empleado.getIdEmpleado() + "] atendiendo pasajero ==");
+        } catch (Exception e) {
+            System.out.println("ERROR con empleado al querer atender al pasajero " + this.idPuesto
+                    + e.getMessage());
+        }
+    }
 
-            System.out.println("== Atendiendo pasajero ==");
-            // TODO ponerle un random y asignarle puerta y terminal a pasajero
-            Thread.sleep(1000);
-
+    public void liberarPasajero() {
+        try {
             pasajeroListo.release();
         } catch (Exception e) {
-            // TODO: handle exception
+            System.out.println("ERROR con empleado al querer liberar al pasajero");
         }
     }
 
     // Metodo pasajero
+    public synchronized boolean entrarCola(Pasajero pasajero) {
+        boolean exito = true;
+
+        try {
+            if (colaDisponibilidad.tryAcquire()) {
+                System.out
+                        .println("Pasajero " + pasajero.getIdPasajero() + " esperando en la cola en puesto de atencion["
+                                + this.idPuesto + "]");
+                // Disminuyo el espacio actual
+            } else {
+                exito = false;
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR con pasajero al querer entrar en cola");
+        }
+
+        return exito;
+    }
 
     public void esperarAtencion(Pasajero pasajero) {
 
         try {
-            System.out.println("Pasajero " + pasajero.getIdPasajero() + " esperando en la cola en puesto de atencion["
-                    + this.idPuesto + "]");
-            colaDisponibilidad.acquire();
-            // Disminuyo el espacio actual
-            ocuparLugarCola();
             // Espero a que el empleado este desocupado
             mostrador.acquire();
             mutexEmpleado.release();
@@ -73,50 +98,23 @@ public class PuestoAtencion {
             pasajeroListo.acquire();
             System.out.println("Pasajero " + pasajero.getIdPasajero() + " saliendo del puesto de atencion["
                     + this.idPuesto + "]");
-            // Es liberado entonces libera el mostrador y un espacio cola
-            liberarLugarCola();
+            // Es liberado entonces libera el mostrador
             mostrador.release();
+            colaDisponibilidad.release();
+            hayLugarCola();
 
         } catch (Exception e) {
-            // TODO: handle exception
+            System.out.println("ERROR con pasajero al salir de puesto de atencion");
         }
     }
 
-    private void ocuparLugarCola() {
+    // Metodos guardia
+    public synchronized void hayLugarCola() {
         try {
-            lock.lock();
-            this.capacidadActual--;
-            System.out.println("Capacidad actual de la cola en el puesto[" + this.idPuesto + "] es de ["
-                    + this.capacidadActual + "]");
-            lock.unlock();
+            walkie.notificarGuardia();
         } catch (Exception e) {
-            System.out.println("ERROR al disminuir capacidad de cola");
+            System.out.println("ERROR avisar lugar disponible a guardia");
         }
-    }
-
-    private void liberarLugarCola() {
-        try {
-            lock.lock();
-            this.capacidadActual++;
-            System.out.println("Capacidad actual de la cola en el puesto[" + this.idPuesto + "] es de ["
-                    + this.capacidadActual + "]");
-            lock.unlock();
-        } catch (Exception e) {
-            System.out.println("ERROR al aumentar capacidad de cola");
-        }
-    }
-
-    // Metodo Guardia
-    public int hayLugarCola() {
-        int capacidad = -1;
-        try {
-            lock.lock();
-            capacidad = this.capacidadActual;
-            lock.unlock();
-        } catch (Exception e) {
-            System.out.println("ERROR al adquirir espacio libre en cola");
-        }
-        return capacidad;
     }
 
 }
