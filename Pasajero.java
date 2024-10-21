20fe58554b034c87f9fa978b32f8ef31326c7f86
Torgeck <54917534+Torgeck@users.noreply.github.com>
2024-10-21 01:54:00 -0300

    Agregado logica a pasajero, 1/3 done

--- Pasajero.java
+++ Pasajero.java
@@ -3,17 +3,13 @@ public class Pasajero implements Runnable {
     private static int ID = 0;
     private int idPasajero;
     private Reserva reserva;
-
-    // Objetos pasivos
     private Aeropuerto aeropuerto;
-    private PuestoInformes informes;
-    private PuestoAtencion atencion;
     private Tren tren;
 
-    public Pasajero(Aeropuerto aeropuerto, Reserva reserva) {
+    public Pasajero(Aeropuerto aeropuerto, String nombreEmpresa) {
         this.idPasajero = generarID();
         this.aeropuerto = aeropuerto;
-        this.reserva = reserva;
+        this.reserva = new Reserva(nombreEmpresa);
     }
 
     private int generarID() {
@@ -40,22 +36,6 @@ public class Pasajero implements Runnable {
         this.aeropuerto = aeropuerto;
     }
 
-    public PuestoInformes getInformes() {
-        return informes;
-    }
-
-    public void setInformes(PuestoInformes informes) {
-        this.informes = informes;
-    }
-
-    public PuestoAtencion getAtencion() {
-        return atencion;
-    }
-
-    public void setAtencion(PuestoAtencion atencion) {
-        this.atencion = atencion;
-    }
-
     public Tren getTren() {
         return tren;
     }
@@ -65,25 +45,32 @@ public class Pasajero implements Runnable {
     }
 
     public void run() {
-
+        PuestoAtencion atencion = null;
         // El aeropuerto se encarga de producir pasajeros de tal a cierta hora
         try {
             // Se dirige al puesto de informes y obtiene el puesto de atencion
-            atencion = informes.obtenerPuestoAtencion(reserva.getEmpresa());
-            Thread.sleep(1000);
+            atencion = aeropuerto.getPuestoInformes().obtenerPuestoAtencion(reserva.getEmpresa());
+            System.out.println(
+                    "El pasajero " + this.idPasajero + " se dirige al puesto de atencion [" + atencion.getIdPuesto()
+                            + "]");
+            Thread.sleep(5000);
         } catch (Exception e) {
             System.out.println("Le exploto la reserva en la cara a pasajero " + this.idPasajero);
         }
 
-        System.out.println(
-                "El pasajero " + this.idPasajero + " se dirige al puesto de atencion [" + this.atencion.toString()
-                        + "]");
-        try {
-            atencion.serAtendido();
-
-        } catch (Exception e) {
-
+        // Si no hay lugar en la cola entra a hall a esperar
+        while (!atencion.entrarCola(this)) {
+            try {
+                System.out.println("Pasajero " + this.idPasajero + "Esperando en hall");
+                aeropuerto.getHall().esperarEnHall();
+            } catch (Exception e) {
+                System.out.println("ERROR al esperar en hall");
+            }
         }
+        atencion.esperarAtencion(this);
+        atencion.salirPuestoAtencion(this);
 
+        System.out.println("Pasajero " + this.idPasajero + " murio");
+        // Luego se va al cole a hacer sus cosas
     }
 }
