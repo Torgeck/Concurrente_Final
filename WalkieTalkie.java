0f73f8dbed74f28dde7ef2425289b51fab7fa1d5
Torgeck <54917534+Torgeck@users.noreply.github.com>
2024-10-21 01:42:57 -0300

    Agregado WT como monitor Guardia-PA

---
+++ WalkieTalkie.java
@@ -0,0 +1,15 @@
+
+public class WalkieTalkie {
+
+    public WalkieTalkie() {
+
+    }
+
+    public synchronized void notificarGuardia() {
+        notify();
+    }
+
+    public synchronized void esperarLlamada() throws InterruptedException {
+        wait();
+    }
+}
