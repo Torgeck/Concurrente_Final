8267289f5ce88a6c450605aada4afa51b4503b42
Torgeck <54917534+Torgeck@users.noreply.github.com>
2024-10-21 01:43:45 -0300

    Agregado Hall como monitor Guardia-Pasajero

---
+++ Hall.java
@@ -0,0 +1,17 @@
+public class Hall {
+
+    private Aeropuerto aeropuerto;
+
+    public Hall(Aeropuerto aeropuerto) {
+        this.aeropuerto = aeropuerto;
+    }
+
+    public synchronized void avisoLugarLibre() {
+        notifyAll();
+    }
+
+    public synchronized void esperarEnHall() throws InterruptedException {
+        wait();
+    }
+
+}
