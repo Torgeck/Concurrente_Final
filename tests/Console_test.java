c6901dade9c9f263715d9b2bcf0ea367d3193d63
Torgeck <54917534+Torgeck@users.noreply.github.com>
2024-10-25 15:31:44 -0300

    Add Console class test

---
+++ tests/Console_test.java
@@ -0,0 +1,10 @@
+package tests;
+
+import console.Console;
+
+public class Console_test {
+
+    public static void main(String[] args) {
+        System.out.printf(Console.colorString("RED","aaaaaaaaaaaaaaaaaa"));
+    }
+}
