b747b6dc4a347d1b44adacf94b63a8c4c47519cf
Torgeck <54917534+Torgeck@users.noreply.github>
2024-10-28 19:29:05 -0300

    Modify constructor and add getter/setter

--- pasivos/Reserva.java
+++ pasivos/Reserva.java
@@ -5,9 +5,17 @@ public class Reserva {
     private String empresa;
     private int puertaEmbarque;
     private char terminal;
+    private long horaEmbarque;
 
     public Reserva(String empresa) {
         this.empresa = empresa;
+    }
+
+    public Reserva(Vuelo vuelo) {
+        this.empresa = vuelo.getAerolinea();
+        this.terminal = vuelo.getTerminal();
+        this.puertaEmbarque = vuelo.getPuertaEmbarque();
+        this.horaEmbarque = vuelo.getHoraEmbarque();
     }
 
     public String getEmpresa() {
@@ -28,6 +36,19 @@ public class Reserva {
 
     public void setTerminal(char terminal) {
         this.terminal = terminal;
+    }
+
+    public long getHoraEmbarque() {
+        return horaEmbarque;
+    }
+
+    public void setHoraEmbarque(long horaEmbarque) {
+        this.horaEmbarque = horaEmbarque;
+    }
+
+    public String toString() {
+        return "[ Emp:" + this.empresa + ", Ter:" + this.terminal + ", P.Emb: " + this.puertaEmbarque + ", H.Emb:"
+                + this.horaEmbarque + "]";
     }
 
 }
