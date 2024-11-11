92c6994b18bb948374c872f5e4ce416a6d1d0478
Torgeck <54917534+Torgeck@users.noreply.github.com>
2024-11-10 23:32:16 -0300

    Add basic logic to exit and enter FS, remove methods and attributes that didn't belong

--- pasivos/Freeshop.java
+++ pasivos/Freeshop.java
@@ -1,84 +1,101 @@
 package pasivos;
 
-import hilos.Cajero;
+import console.Console;
 
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Freeshop {
 
-    private Cajero[] cajero;
+    private Caja[] cajas;
     private int capMax;
     private int capActual;
+    private char idFreeshop;
+    private Semaphore semCapacidad;
     private ReentrantLock modificar;
     private Random random;
 
-    public Freeshop(Cajero[] cajero) {
-        this.cajero = cajero;
+    public Freeshop(char idTerminal, int cantCajas, int capMax) {
+        this.idFreeshop = idTerminal;
+        this.cajas = new Caja[cantCajas];
+        this.capMax = capMax;
+        this.capActual = 0;
+        this.semCapacidad = new Semaphore(capMax);  // No necesita ser fair
+        this.random = new Random();
+        this.modificar = new ReentrantLock();
+        iniCajas();
+    }
+
+    private void iniCajas() {
+        // Metodo que crea y asigna cajas creadas al arreglo de cajas
+        Caja caja;
+        for (int i = 0; i < this.cajas.length; i++) {
+            caja = new Caja(this.idFreeshop);
+            this.cajas[i] = caja;
+        }
+    }
+
+    public Caja[] getCajas() {
+        return cajas;
     }
 
     // Metodos para modificar y checkear variables
-    public int getCapActual() {
+    private int getCapActual() {
         int respuesta = -1;
-
+        modificar.lock();
         try {
-            modificar.lock();
             respuesta = capActual;
         } catch (Exception e) {
             System.out.println("ERROR al querer acceder a la variable capActual en Freeshop");
-            ;
         } finally {
             modificar.unlock();
         }
         return respuesta;
     }
 
-    public void disminuirCapActual() {
+    private void modificarCapActual(int x) {
+        modificar.lock();
         try {
-            modificar.lock();
-            capActual--;
+            this.capActual += x;
+            System.out.println(Console.colorString("PURPLE", "CAPACIDAD ACTUAL FS" + this.idFreeshop + ": " + this.capActual + " DE MAXIMO: " + this.capMax));
         } catch (Exception e) {
-            System.out.println("ERROR al querer disminuir capActual en Freeshop");
+            System.out.println(Console.colorString("RED", "ERROR al querer aumentar capActual en Freeshop"));
         } finally {
             modificar.unlock();
         }
     }
 
-    public void aumentarCapActual() {
-        try {
-            modificar.lock();
-            capActual++;
-        } catch (Exception e) {
-            System.out.println("ERROR al querer aumentar capActual en Freeshop");
-        } finally {
-            modificar.unlock();
-        }
-    }
-
-    // TODO hacer toda la logica para freeshop
     // Metodos pasajeros
-    public void ingresarFreeshop() {
+    public boolean ingresarFreeshop() {
+        boolean compra = false;
+        try {
+            // Ingresa al FS
+            semCapacidad.acquire();
+            // Aumenta la capacidad actual
+            modificarCapActual(1);
+            compra = this.hizoCompra();
+        } catch (InterruptedException e) {
+            System.out.println(Console.colorString("RED", "ERROR al querer ingresar freeshop"));
+        }
+        return compra;
     }
 
     public void salirFreeshop() {
+        // Reduce la capacidad actual
+        modificarCapActual(-1);
+        // Libera un semCapacida
+        semCapacidad.release();
     }
 
-    public boolean hizoCompra() {
+    private boolean hizoCompra() {
         // Hace un 50/50 para saber si pasajero quiere comprar en el freeshop
         return random.nextBoolean();
     }
 
-    public void esperarCajero() {
-    }
-
-    public void liberarCajero() {
-    }
-
-    // Metodos cajeros
-    public void esperarCliente() {
-    }
-
-    public void liberarCliente() {
+    public Caja getCajaMenosOcupada() {
+        // Retorna la caja con menor fila
+        return this.cajas[0].getCantClientes() <= this.cajas[1].getCantClientes() ? this.cajas[0] : this.cajas[1];
     }
 
 
