79176aa77387cdefa5f679873c866887474f3043
Torgeck <54917534+Torgeck@users.noreply.github.com>
2024-11-12 03:02:49 -0300

    Delete unused code, add Reloj as an attribute

--- pasivos/Terminal.java
+++ pasivos/Terminal.java
@@ -1,6 +1,6 @@
 package pasivos;
 
-import console.Console;
+import hilos.Reloj;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,14 +9,16 @@ import java.util.Random;
 public class Terminal {
 
     private Aeropuerto aeropuerto;
+    private Reloj reloj;
     private char idTerminal;
     private Freeshop freeshop;
     private ArrayList<Integer> arrayPuestosEmbarques;
     private HashMap<Integer, PuestoEmbarque> mapPuestoEmbarques;
     private Random random;
 
-    public Terminal(Aeropuerto aeropuerto, char idTerminal, int limiteInferior, int limiteSuperior, int cantCajas, int capMax) {
+    public Terminal(Aeropuerto aeropuerto, Reloj reloj, char idTerminal, int limiteInferior, int limiteSuperior, int cantCajas, int capMax) {
         this.aeropuerto = aeropuerto;
+        this.reloj = reloj;
         this.idTerminal = idTerminal;
         this.mapPuestoEmbarques = new HashMap<Integer, PuestoEmbarque>();
         this.arrayPuestosEmbarques = new ArrayList<>();
@@ -43,21 +45,12 @@ public class Terminal {
 
     public void generarPuestoEmbarque(int limiteInferior, int limiteSuperior) {
         for (int i = limiteInferior; i <= limiteSuperior; i++) {
-            this.mapPuestoEmbarques.put(i, new PuestoEmbarque(i, this));
+            this.mapPuestoEmbarques.put(i, new PuestoEmbarque(i, this, this.reloj));
             arrayPuestosEmbarques.add(i);
         }
     }
 
     public PuestoEmbarque getPuestoRandom() {
         return mapPuestoEmbarques.get(arrayPuestosEmbarques.get(random.nextInt(arrayPuestosEmbarques.size())));
-    }
-
-    public synchronized void esperarLlamado() {
-        // TODO tendrian que esperar en su puerta
-        try {
-            wait();
-        } catch (InterruptedException e) {
-            System.out.println(Console.colorString("RED", "ERROR con el pasajero al esperar"));
-        }
     }
 }
