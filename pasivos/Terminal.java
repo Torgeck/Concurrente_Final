e237d21b29178392b7fa2508c633aa80a626e83b
Torgeck <54917534+Torgeck@users.noreply.github.com>
2024-11-26 03:40:42 -0300

    Refactor code to follow SOLID principles

--- pasivos/Terminal.java
+++ pasivos/Terminal.java
@@ -9,16 +9,14 @@ import java.util.Random;
 public class Terminal {
 
     private Aeropuerto aeropuerto;
-    private Reloj reloj;
     private char idTerminal;
     private Freeshop freeshop;
     private ArrayList<Integer> arrayPuestosEmbarques;
     private HashMap<Integer, PuestoEmbarque> mapPuestoEmbarques;
     private Random random;
 
-    public Terminal(Aeropuerto aeropuerto, Reloj reloj, char idTerminal, int limiteInferior, int limiteSuperior, int cantCajas, int capMax) {
+    public Terminal(Aeropuerto aeropuerto, char idTerminal, int limiteInferior, int limiteSuperior, int cantCajas, int capMax) {
         this.aeropuerto = aeropuerto;
-        this.reloj = reloj;
         this.idTerminal = idTerminal;
         this.mapPuestoEmbarques = new HashMap<Integer, PuestoEmbarque>();
         this.arrayPuestosEmbarques = new ArrayList<>();
@@ -45,12 +43,20 @@ public class Terminal {
 
     public void generarPuestoEmbarque(int limiteInferior, int limiteSuperior) {
         for (int i = limiteInferior; i <= limiteSuperior; i++) {
-            this.mapPuestoEmbarques.put(i, new PuestoEmbarque(i, this, this.reloj));
+            this.mapPuestoEmbarques.put(i, new PuestoEmbarque(i, this));
             arrayPuestosEmbarques.add(i);
         }
     }
 
     public PuestoEmbarque getPuestoRandom() {
         return mapPuestoEmbarques.get(arrayPuestosEmbarques.get(random.nextInt(arrayPuestosEmbarques.size())));
+    }
+
+    public void agregarAlarma(Alarma alarma) {
+        this.aeropuerto.agregarAlarma(alarma);
+    }
+
+    public int getTiempoActual() {
+        return this.aeropuerto.getHora();
     }
 }
