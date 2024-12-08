410d053540c223f9b4670140100ad04a2b99b1d7
Torgeck <54917534+Torgeck@users.noreply.github>
2024-12-08 03:56:25 -0300

    Format code

--- hilos/Guardia.java
+++ hilos/Guardia.java
@@ -24,24 +24,28 @@ public class Guardia implements Runnable {
 
     public void run() {
         System.out.println(Console.colorString(this.color, "Guardia entro a laburar"));
-        while (!this.aeropuerto.estaCerrado()) {
+        while (!this.aeropuerto.getFlagSimulacion()) {
             if (this.aeropuerto.estaCerradoAlPublico()) {
-                System.out.println(Console.colorString(this.color, "\uD83D\uDDE3️\uD83D\uDCE3 Aviso al hall que cerro Aeropuerto por hoy \uD83D\uDDE3️\uD83D\uDCE3"));
+                System.out.println(Console.colorString(this.color,
+                        "\uD83D\uDDE3️\uD83D\uDCE3 Aviso al hall que cerro Aeropuerto por hoy \uD83D\uDDE3️\uD83D\uDCE3"));
                 this.aeropuerto.getHall().avisoLugarLibre();
                 this.aeropuerto.esperarApertura();
                 System.out.println(Console.colorString(this.color, "Guardia entro a laburar"));
             } else {
                 try {
-                    System.out.println(Console.colorString(this.color, idGuardia + " esperando en el hall \uD83D\uDCA4\uD83D\uDCA4"));
+                    System.out.println(Console.colorString(this.color,
+                            idGuardia + " esperando en el hall \uD83D\uDCA4\uD83D\uDCA4"));
                     walkie.esperarLlamada();
-                    System.out.println(Console.colorString(this.color, "\uD83D\uDDE3️\uD83D\uDCE3 Aviso al hall que se desocupo una cola \uD83D\uDDE3️\uD83D\uDCE3"));
+                    System.out.println(Console.colorString(this.color,
+                            "\uD83D\uDDE3️\uD83D\uDCE3 Aviso al hall que se desocupo una cola \uD83D\uDDE3️\uD83D\uDCE3"));
                     aeropuerto.getHall().avisoLugarLibre();
                 } catch (Exception e) {
                     System.out.println(Console.colorString("RED", "ERROR con el guardia en el hall"));
                 }
             }
         }
-        System.out.println(Console.colorString("BLACK", "\uD83C\uDFC4\uD83C\uDFC4 Guardia se toma vacaciones por siempre \uD83C\uDFC4\uD83C\uDFC4"));
+        System.out.println(Console.colorString("BLACK",
+                "\uD83C\uDFC4\uD83C\uDFC4 Guardia se toma vacaciones por siempre \uD83C\uDFC4\uD83C\uDFC4"));
     }
 
 }
