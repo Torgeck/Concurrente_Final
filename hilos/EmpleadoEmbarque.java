629db91f032db2e07c07eb714affb7d3a5a10692
Torgeck <54917534+Torgeck@users.noreply.github>
2024-12-08 03:55:33 -0300

    Add logic to wait for closing time

--- hilos/EmpleadoEmbarque.java
+++ hilos/EmpleadoEmbarque.java
@@ -23,26 +23,34 @@ public class EmpleadoEmbarque implements Runnable {
 
     public void run() {
 
-        while (!this.aeropuerto.estaCerrado()) {
+        while (!this.aeropuerto.getFlagSimulacion()) {
 
             if (this.aeropuerto.estaCerradoAlPublico() || !puesto.hayVuelos()) {
-                System.out.println(Console.colorString("WHITE", "Empleado embarque avisa que va a cerrar su puesto"));
-                this.puesto.avisarPasajerosEmbarque();
-                System.out.println(Console.colorString("WHITE", "Empleado embarque, Aeropuerto cerrado / No mas vuelos, me voy a casa"));
+                this.puesto.esperarCierre();
+                System.out.println(Console.colorString("WHITE",
+                        "Empleado embarque" + this.idEmpleado + " avisa que va a cerrar su puesto"));
+                this.puesto.avisarPasajerosCierre();
+                System.out.println(Console.colorString("WHITE",
+                        "Empleado embarque" + this.idEmpleado + " Aeropuerto cerrado, me voy a casa"));
                 this.aeropuerto.esperarApertura();
             } else {
-                System.out.println(Console.colorString("YELLOW", "Empleado embarque " + this.idEmpleado + " entra a laburar"));
+                System.out.println(
+                        Console.colorString("YELLOW", "Empleado embarque " + this.idEmpleado + " entra a laburar"));
                 while (puesto.hayVuelos()) {
                     puesto.actualizarVuelo();
-                    System.out.println(Console.colorString("YELLOW", "Empleado embarque " + this.idEmpleado + " crea recordatorio embarque del vuelo"));
+                    System.out.println(Console.colorString("YELLOW",
+                            "Empleado embarque " + this.idEmpleado + " crea recordatorio embarque del vuelo"));
                     puesto.ponerAlarma();
-                    System.out.println(Console.colorString("YELLOW", "Empleado embarque " + this.idEmpleado + " avisa a pasajeros para el embarque del vuelo: " + puesto.getVueloActual()));
+                    System.out.println(Console.colorString("YELLOW", "Empleado embarque " + this.idEmpleado
+                            + " avisa a pasajeros para el embarque del vuelo: " + puesto.getVueloActual()));
                     puesto.avisarPasajerosEmbarque();
                 }
-                System.out.println(Console.colorString("YELLOW", "Empleado embarque " + this.idEmpleado + " no hay mas vuelos para este puesto por hoy"));
+                System.out.println(Console.colorString("YELLOW",
+                        "Empleado embarque " + this.idEmpleado + " no hay mas vuelos para este puesto por hoy"));
             }
         }
-        System.out.println(Console.colorString("BLACK", "\uD83C\uDFC4\uD83C\uDFC4 Empleado de embarque " + this.idEmpleado + " se toma vacaciones por siempre \uD83C\uDFC4\uD83C\uDFC4"));
+        System.out.println(Console.colorString("BLACK", "\uD83C\uDFC4\uD83C\uDFC4 Empleado de embarque "
+                + this.idEmpleado + " se toma vacaciones por siempre \uD83C\uDFC4\uD83C\uDFC4"));
     }
 
 }
