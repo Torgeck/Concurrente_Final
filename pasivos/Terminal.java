089b223e889549b069e86c1e42441b32077fc937
Torgeck <54917534+Torgeck@users.noreply.github>
2024-12-08 04:07:28 -0300

    Add methods for closing time

--- pasivos/Terminal.java
+++ pasivos/Terminal.java
@@ -1,10 +1,9 @@
 package pasivos;
 
-import hilos.Reloj;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
+import java.util.concurrent.CyclicBarrier;
 
 public class Terminal {
 
@@ -14,8 +13,10 @@ public class Terminal {
     private ArrayList<Integer> arrayPuestosEmbarques;
     private HashMap<Integer, PuestoEmbarque> mapPuestoEmbarques;
     private Random random;
+    private CyclicBarrier cierre;
 
-    public Terminal(Aeropuerto aeropuerto, char idTerminal, int limiteInferior, int limiteSuperior, int cantCajas, int capMax) {
+    public Terminal(Aeropuerto aeropuerto, char idTerminal, int limiteInferior, int limiteSuperior, int cantCajas,
+            int capMax) {
         this.aeropuerto = aeropuerto;
         this.idTerminal = idTerminal;
         this.mapPuestoEmbarques = new HashMap<Integer, PuestoEmbarque>();
@@ -23,22 +24,27 @@ public class Terminal {
         this.random = new Random();
         this.freeshop = new Freeshop(idTerminal, cantCajas, capMax);
         generarPuestoEmbarque(limiteInferior, limiteSuperior);
+        this.cierre = new CyclicBarrier(arrayPuestosEmbarques.size());
     }
 
     public char getId() {
-        return idTerminal;
+        return this.idTerminal;
+    }
+
+    public Aeropuerto getAeropuerto() {
+        return this.aeropuerto;
     }
 
     public Freeshop getFreeshop() {
-        return freeshop;
+        return this.freeshop;
     }
 
     public ArrayList<Integer> getArrayPuestosEmbarques() {
-        return arrayPuestosEmbarques;
+        return this.arrayPuestosEmbarques;
     }
 
     public HashMap<Integer, PuestoEmbarque> getMapPuestoEmbarques() {
-        return mapPuestoEmbarques;
+        return this.mapPuestoEmbarques;
     }
 
     public void generarPuestoEmbarque(int limiteInferior, int limiteSuperior) {
@@ -58,5 +64,23 @@ public class Terminal {
 
     public int getTiempoActual() {
         return this.aeropuerto.getHora();
+    }
+
+    public void esperarCierre() {
+        try {
+            cierre.await();
+        } catch (Exception e) {
+            System.out.println("ERROR al esperar cierre empleados");
+        }
+    }
+
+    public void nuevoDia() {
+        cierre.reset();
+    }
+
+    public void liberarEmpleadosEmbarque() {
+        this.mapPuestoEmbarques.forEach((k, puesto) -> {
+            puesto.liberarEmpleado();
+        });
     }
 }
