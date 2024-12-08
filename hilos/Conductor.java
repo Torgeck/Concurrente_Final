6c229efeb047d7a750cd8b174d2de3a482988b91
Torgeck <54917534+Torgeck@users.noreply.github>
2024-12-08 03:53:24 -0300

    Fix logic for edge cases

--- hilos/Conductor.java
+++ hilos/Conductor.java
@@ -1,6 +1,7 @@
 package hilos;
 
 import console.Console;
+import customExceptions.AeropuertoCerradoException;
 import pasivos.Aeropuerto;
 import pasivos.Tren;
 
@@ -13,45 +14,56 @@ public class Conductor implements Runnable {
     public Conductor(Tren tren, Aeropuerto aeropuerto) {
         this.tren = tren;
         this.aeropuerto = aeropuerto;
-        this.tiempoViaje = 1000 * 4;
+        this.tiempoViaje = 1000 * 1;
     }
 
     public void run() {
         int terminalActual;
 
-        while (!this.aeropuerto.estaCerrado()) {
+        while (!this.aeropuerto.getFlagSimulacion()) {
 
             if (this.aeropuerto.estaCerradoAlPublico()) {
-                System.out.println(Console.colorString("YELLOW", "Conductor notifica pasajeros esperando que cerro el aeropuerto"));
+                System.out.println(Console.colorString("YELLOW",
+                        "Conductor notifica pasajeros esperando que cerro por hoy el aeropuerto"));
                 this.tren.avisarPasajerosCierre();
                 System.out.println(Console.colorString("WHITE", "Conductor, Aeropuerto cerrado me voy a casa"));
                 this.aeropuerto.esperarApertura();
             } else {
 
                 terminalActual = 0;
-                tren.esperarEnAeropuerto();
+                try {
+                    tren.esperarEnAeropuerto();
+                } catch (AeropuertoCerradoException e) {
+                    terminalActual = -1;
+                    tren.reset();
+                }
 
-                while (terminalActual < 3) {
-                    tren.irSiguienteTerminal();
+                if (terminalActual >= 0) {
+                    while (terminalActual < 3) {
+                        tren.irSiguienteTerminal();
+                        try {
+                            Thread.sleep(this.tiempoViaje);
+                            tren.avisarPasajerosParada();
+                        } catch (Exception e) {
+                            System.out.println(Console.colorString("RED", "ERROR al ir a una terminal"));
+                        }
+                        tren.esperarDesembarquePasajeros();
+                        terminalActual++;
+                    }
+                    tren.irAeropuerto();
                     try {
                         Thread.sleep(this.tiempoViaje);
-                        tren.avisarPasajerosParada();
-                    } catch (Exception e) {
-                        System.out.println(Console.colorString("RED", "ERROR al ir a una terminal"));
+                    } catch (InterruptedException e) {
+                        System.out.println(Console.colorString("RED", "ERROR al volver a aeropuerto"));
                     }
-                    tren.esperarDesembarquePasajeros();
-                    terminalActual++;
                 }
 
-                tren.irAeropuerto();
-                try {
-                    Thread.sleep(this.tiempoViaje);
-                } catch (InterruptedException e) {
-                    System.out.println(Console.colorString("RED", "ERROR al volver a aeropuerto"));
-                }
             }
         }
-        System.out.println(Console.colorString("BLACK", "\uD83C\uDFC4\uD83C\uDFC4 Conductor se toma vacaciones por siempre \uD83C\uDFC4\uD83C\uDFC4"));
+        System.out.println(Console.colorString("YELLOW",
+                "Conductor notifica pasajeros esperando que cerro permanentemente el aeropuerto"));
+        this.tren.avisarPasajerosCierre();
+        System.out.println(Console.colorString("BLACK",
+                "\uD83C\uDFC4\uD83C\uDFC4 Conductor se toma vacaciones por siempre \uD83C\uDFC4\uD83C\uDFC4"));
     }
 }
-
