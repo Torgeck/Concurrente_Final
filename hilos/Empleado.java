752340cd829c78fbf3227963bc560320cac6b43e
Torgeck <54917534+Torgeck@users.noreply.github>
2024-12-08 03:54:11 -0300

    Fix logic for edge cases

--- hilos/Empleado.java
+++ hilos/Empleado.java
@@ -33,31 +33,44 @@ public class Empleado implements Runnable {
     public void run() {
 
         System.out.println("Empleado " + this.idEmpleado + " se dirige al puesto " + puesto.getIdPuesto());
-        while (!this.aeropuerto.estaCerrado()) {
+        while (!this.aeropuerto.getFlagSimulacion()) {
 
             if (this.aeropuerto.estaCerradoAlPublico()) {
-                System.out.println(Console.colorString("PURPLE", "Empleado " + this.idEmpleado + " se fija si quedan pasajeros esperando en cola"));
+                System.out.println(Console.colorString("PURPLE",
+                        "Empleado " + this.idEmpleado + " se fija si quedan pasajeros esperando en cola"));
                 // Avisa a los pasajeros que ya cerro el aeropuerto
                 while (puesto.hayPasajerosEsperando()) {
                     puesto.atenderPasajero(this);
                     puesto.liberarPasajero();
                 }
 
-                System.out.println(Console.colorString("WHITE", "Empleado en puesto atencion " + puesto.getIdPuesto() + ", Aerpuerto cerrado me voy a casa"));
-                this.aeropuerto.esperarApertura();
-                this.puesto.abrirPuesto();
-                System.out.println("Empleado " + this.idEmpleado + " se dirige al puesto " + puesto.getIdPuesto());
-            } else {
-                this.puesto.atenderPasajero(this);
-                try {
-                    Thread.sleep((random.nextInt(8) + 1) * 1000);
-                } catch (Exception e) {
-                    System.out.println("ERROR exploto el empleado [" + this.idEmpleado + "]");
+                System.out.println(Console.colorString("WHITE",
+                        "Empleado en puesto atencion " + puesto.getIdPuesto() + ", Aeropuerto cerrado me voy a casa"));
+
+                if (!this.aeropuerto.getFlagSimulacion()) {
+                    this.aeropuerto.esperarApertura();
+                    this.puesto.abrirPuesto();
+                    System.out.println("Empleado " + this.idEmpleado + " se dirige al puesto " + puesto.getIdPuesto());
                 }
 
-                puesto.liberarPasajero();
+            } else {
+
+                if (this.puesto.atenderPasajero(this)) {
+                    try {
+                        Thread.sleep((random.nextInt(8) + 1) * 1000);
+                    } catch (Exception e) {
+                        System.out.println("ERROR exploto el empleado [" + this.idEmpleado + "]");
+                    }
+
+                    puesto.liberarPasajero();
+                } else {
+
+                    System.out.println(Console.colorString("PURPLE",
+                            "Empleado " + this.idEmpleado + " se tomo un recreo"));
+                }
             }
         }
-        System.out.println(Console.colorString("BLACK", "\uD83C\uDFC4\uD83C\uDFC4 Empleado de puesto de atencion " + this.idEmpleado + " se toma vacaciones por siempre \uD83C\uDFC4\uD83C\uDFC4"));
+        System.out.println(Console.colorString("BLACK", "\uD83C\uDFC4\uD83C\uDFC4 Empleado de puesto de atencion "
+                + this.idEmpleado + " se toma vacaciones por siempre \uD83C\uDFC4\uD83C\uDFC4"));
     }
 }
