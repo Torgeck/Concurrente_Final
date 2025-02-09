ce415eb21df07572e66c6dc9698c5a79ab5fbe84
Torgeck <54917534+Torgeck@users.noreply.github.com>
2025-02-09 00:30:35 -0300

    Add new methods for Pasajero

--- pasivos/Hall.java
+++ pasivos/Hall.java
@@ -5,11 +5,21 @@ public class Hall {
     public Hall() {
     }
 
+    // Metodos Pasajero
+    public synchronized void notificarGuardia() {
+        notify();
+    }
+
+    public synchronized void esperarEnHall() throws InterruptedException {
+        wait();
+    }
+
+    // Metodos Guardia
     public synchronized void avisoLugarLibre() {
         notifyAll();
     }
 
-    public synchronized void esperarEnHall() throws InterruptedException {
+    public synchronized void esperarLlamada() throws InterruptedException {
         wait();
     }
 
