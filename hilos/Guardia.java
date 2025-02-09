1529dfa83371c63980cbbeafa4c4e8865a79bf77
Torgeck <54917534+Torgeck@users.noreply.github.com>
2025-02-09 00:21:38 -0300

    Replace WalkieTakie class with Hall

--- hilos/Guardia.java
+++ hilos/Guardia.java
@@ -2,24 +2,20 @@ package hilos;
 
 import console.Console;
 import pasivos.Aeropuerto;
-import pasivos.WalkieTalkie;
+import pasivos.Hall;
 
 public class Guardia implements Runnable {
 
     private final String idGuardia;
     private final String color;
     private final Aeropuerto aeropuerto;
-    private final WalkieTalkie walkie;
+    private final Hall hall;
 
     public Guardia(Aeropuerto aeropuerto) {
         this.aeropuerto = aeropuerto;
-        this.walkie = new WalkieTalkie();
+        this.hall = aeropuerto.getHall();
         this.idGuardia = "Guardia de hall";
         this.color = "CYAN";
-    }
-
-    public WalkieTalkie getWalkie() {
-        return this.walkie;
     }
 
     public void run() {
@@ -28,17 +24,17 @@ public class Guardia implements Runnable {
             if (this.aeropuerto.estaCerradoAlPublico()) {
                 System.out.println(Console.colorString(this.color,
                         "\uD83D\uDDE3️\uD83D\uDCE3 Aviso al hall que cerro Aeropuerto por hoy \uD83D\uDDE3️\uD83D\uDCE3"));
-                this.aeropuerto.getHall().avisoLugarLibre();
+                this.hall.avisoLugarLibre();
                 this.aeropuerto.esperarApertura();
                 System.out.println(Console.colorString(this.color, "Guardia entro a laburar"));
             } else {
                 try {
                     System.out.println(Console.colorString(this.color,
                             idGuardia + " esperando en el hall \uD83D\uDCA4\uD83D\uDCA4"));
-                    walkie.esperarLlamada();
+                    this.hall.esperarLlamada();
                     System.out.println(Console.colorString(this.color,
                             "\uD83D\uDDE3️\uD83D\uDCE3 Aviso al hall que se desocupo una cola \uD83D\uDDE3️\uD83D\uDCE3"));
-                    aeropuerto.getHall().avisoLugarLibre();
+                    this.hall.avisoLugarLibre();
                 } catch (Exception e) {
                     System.out.println(Console.colorString("RED", "ERROR con el guardia en el hall"));
                 }
